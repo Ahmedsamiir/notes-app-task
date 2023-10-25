ab63f2aa6d896086749216dd64cfec13e75da5a3
mac <Ahmedsamir22081997@gmail.com>
2023-10-25 17:29:23 +0400

    Finished Notes app

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app_task/constants.dart';
+import 'package:notes_app_task/cubits/add_note_cubit/add_note_state.dart';
+import 'package:notes_app_task/models/note_model.dart';
+
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+
+  Color color = const Color(0xffAC3931);
+  addNote(NoteModel note) async {
+    note.color = color.value;
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
+}
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app_task/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app_task/views/widgets/note_list_view.dart';
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatefulWidget {
+  const NotesViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Notes',
+            icon: Icons.search,
+          ),
+          Expanded(
+            child: NotesListView(),
+          ),
+        ],
+      ),
+    );
+  }
+}
